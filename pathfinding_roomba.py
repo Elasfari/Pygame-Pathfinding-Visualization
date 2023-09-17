e033a9ede4466c4a23aa881a62ddc3320f635319
Yassin Elasfar <95232925+Elasfari@users.noreply.github.com>
2023-09-17 20:43:51 +0300

    Add files via upload

---
+++ pathfinding_roomba.py
@@ -0,0 +1,176 @@
+import pygame, sys
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+from pathfinding.core.diagonal_movement import DiagonalMovement
+
+
+class Pathfinder:
+    def __init__(self, matrix):
+        # setup
+        self.matrix = matrix
+        self.grid = Grid(matrix=matrix)
+        self.select_surf = pygame.image.load('selection.png').convert_alpha()
+
+        # pathfinding
+        self.path = []
+
+        # Car
+        self.car = pygame.sprite.GroupSingle(Car(self.empty_path))
+
+    def empty_path(self):
+        self.path = []
+
+    def draw_active_cell(self):
+        mouse_pos = pygame.mouse.get_pos()
+        row = mouse_pos[1] // 32
+        col = mouse_pos[0] // 32
+        current_cell_value = self.matrix[row][col]
+        if current_cell_value == 1:
+            rect = pygame.Rect((col * 32, row * 32), (32, 32))
+            screen.blit(self.select_surf, rect)
+
+    def create_path(self):
+        # start
+        start_x, start_y = self.car.sprite.get_coord()
+        start = self.grid.node(start_x, start_y)
+
+        # end
+        mouse_pos = pygame.mouse.get_pos()
+        end_x, end_y = mouse_pos[0] // 32, mouse_pos[1] // 32
+        end = self.grid.node(end_x, end_y)
+
+        # path
+        finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+        self.path, runs = finder.find_path(start, end, self.grid)
+        self.grid.cleanup()
+        self.car.sprite.set_path(self.path)
+        print(runs)
+
+    def draw_path(self):
+        if self.path and len(self.path) >= 2:
+            points = []
+            for node in self.path:
+                x, y = node.x, node.y  # Get the coordinates of the GridNode
+                x = (x * 32) + 16
+                y = (y * 32) + 16
+                points.append((x, y))
+
+            if len(points) >= 2:
+                pygame.draw.lines(screen, '#FFFFFF', False, points, 5)
+
+    def update(self):
+        self.draw_active_cell()
+        self.draw_path()
+
+        # car updating and drawing
+        self.car.update()
+        self.car.draw(screen)
+
+
+class Car(pygame.sprite.Sprite):
+    def __init__(self, empty_path):
+
+        # basic
+        super().__init__()
+        self.image = pygame.image.load('roomba.png').convert_alpha()
+        self.rect = self.image.get_rect(center=(80, 684))
+
+        # movement
+        self.pos = self.rect.center
+        self.speed = 3.5
+        self.direction = pygame.math.Vector2(0, 0)
+
+        # path
+        self.path = []
+        self.collision_rects = []
+        self.empty_path = empty_path
+
+    def get_coord(self):
+        col = self.rect.centerx // 32
+        row = self.rect.centery // 32
+        return col, row
+
+    def set_path(self, path):
+        self.path = path
+        self.create_collision_rects()
+        self.get_direction()
+
+    def create_collision_rects(self):
+        if self.path:
+            self.collision_rects = []
+            for node in self.path:
+                x, y = node.x, node.y  # Get the coordinates of the GridNode
+                x = (x * 32) + 16
+                y = (y * 32) + 16
+                rect = pygame.Rect((x - 4, y - 4), (8, 8))
+                self.collision_rects.append(rect)
+
+    def get_direction(self):
+        if self.collision_rects:
+            start = pygame.math.Vector2(self.pos)
+            end = pygame.math.Vector2(self.collision_rects[0].center)
+            self.direction = (end - start).normalize()
+        else:
+            self.direction = pygame.math.Vector2(0, 0)
+            self.path = []
+
+    def check_collisions(self):
+        if self.collision_rects:
+            for rect in self.collision_rects:
+                if rect.collidepoint(self.pos):
+                    del self.collision_rects[0]
+                    self.get_direction()
+        else:
+            self.empty_path()
+
+    def update(self):
+        self.pos += self.direction * self.speed
+        self.check_collisions()
+        self.rect.center = self.pos
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 704))
+clock = pygame.time.Clock()
+
+# game setup
+bg_surf = pygame.image.load('newmap.jpeg').convert()
+matrix = [
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],]
+pathfinder = Pathfinder(matrix)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pathfinder.create_path()
+
+    screen.blit(bg_surf, (0, 0))
+    pathfinder.update()
+
+    pygame.display.update()
+    clock.tick(60)
